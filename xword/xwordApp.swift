37b15b44873d40fd5de567b196a7f913b2e41505
Jack Crane <jack@jackcrane.rocks>
2026-04-06 10:53:38 -0400

    Initial Commit

---
+++ xword/xwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  xwordApp.swift
+//  xword
+//
+//  Created by Jack Crane on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct xwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xwordTests/xwordTests.swift
@@ -0,0 +1,17 @@
+//
+//  xwordTests.swift
+//  xwordTests
+//
+//  Created by Jack Crane on 4/6/26.
+//
+
+import Testing
+@testable import xword
+
+struct xwordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
